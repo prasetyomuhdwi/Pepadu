1ae843c24eedf12cbdafa73ec960999863355069
prasetyomuhdwi <muhammad.2365@students.amikom.ac.id>
2025-11-22 22:41:30 +0800

    add custome css

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/index.css", "resources/js/app.js"],
+            refresh: ["resources/views/**", "routes/**", "resources/routes/**"],
         }),
         tailwindcss(),
+        {
+            name: "blade",
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith(".blade.php")) {
+                    server.ws.send({
+                        type: "full-reload",
+                        path: "*",
+                    });
+                }
+            },
+        },
     ],
 });
